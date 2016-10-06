0745568cb211c00fc65828f0cacea90cdfc305d8
Cyril David <cyx@cyx.is>
2016-10-06 15:14:56 -0700

    Randomize port we use via get-random-port
    Also publish a HEROKU_STATSD_PORT variable.

--- .profile.d/statsdaemon.sh
+++ .profile.d/statsdaemon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-statsdaemon -export-url $HEROKU_METRICS_URL -instance $DYNO &
+export HEROKU_STATSD_PORT=$(get-random-port)
+statsdaemon -address ":$HEROKU_STATSD_PORT" -export-url $HEROKU_METRICS_URL -instance $DYNO &
