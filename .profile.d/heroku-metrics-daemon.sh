79d90eef64fd1604d94fe708819ccaef2fdd1635
Cyril David <cyx@cyx.is>
2016-10-13 13:35:49 -0700

    Use HEROKU_PROM_* prefix for everything prometheus related

--- .profile.d/heroku-metrics-daemon.sh
+++ .profile.d/heroku-metrics-daemon.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-export HEROKU_METRICS_ENDPOINT=${HEROKU_METRICS_ENDPOINT:-/metrics}
-export HEROKU_METRICS_POLL_INTERVAL=${HEROKU_METRICS_POLL_INTERVAL:-5}
+export HEROKU_PROM_METRICS_ENDPOINT=${HEROKU_PROM_METRICS_ENDPOINT:-/metrics}
+export HEROKU_PROM_METRICS_PORT=$(get-random-port)
+export HEROKU_PROM_METRICS_POLL_INTERVAL=${HEROKU_PROM_METRICS_POLL_INTERVAL:-5}
 
 if [ -z "$HEROKU_METRICS_USE_STATSD" ]; then
 	metrics-poller \
-		-scrape-url "http://localhost:${PORT}${HEROKU_METRICS_ENDPOINT}" \
+		-scrape-url "http://localhost:${HEROKU_PROM_METRICS_PORT}${HEROKU_PROM_METRICS_ENDPOINT}" \
 		-url $HEROKU_METRICS_URL \
 		-instance ${DYNO} \
-		-interval ${HEROKU_METRICS_POLL_INTERVAL} &
+		-interval ${HEROKU_PROM_METRICS_POLL_INTERVAL} &
 else
 	export HEROKU_STATSD_PORT=$(get-random-port)
 	statsdaemon -address ":${HEROKU_STATSD_PORT}" -export-url ${HEROKU_METRICS_URL} -instance ${DYNO} &
