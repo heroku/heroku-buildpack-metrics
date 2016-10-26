7fc55f6c2452b6ebd5e2af1c25b71ee78d4cb626
Cyril David <cyx@cyx.is>
2016-10-26 09:22:59 -0700

    Try ./bin

--- .profile.d/heroku-metrics-daemon.sh
+++ .profile.d/heroku-metrics-daemon.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 export HEROKU_PROM_METRICS_ENDPOINT=${HEROKU_PROM_METRICS_ENDPOINT:-/metrics}
-export HEROKU_PROM_METRICS_PORT=$(get-random-port)
+export HEROKU_PROM_METRICS_PORT=$(./bin/get-random-port)
 export HEROKU_PROM_METRICS_POLL_INTERVAL=${HEROKU_PROM_METRICS_POLL_INTERVAL:-5}
 
 if [ -z "$HEROKU_METRICS_USE_STATSD" ]; then
-	metrics-poller \
+	./bin/metrics-poller \
 		-scrape-url "http://localhost:${HEROKU_PROM_METRICS_PORT}${HEROKU_PROM_METRICS_ENDPOINT}" \
 		-url $HEROKU_METRICS_URL \
 		-instance ${DYNO} \
 		-interval ${HEROKU_PROM_METRICS_POLL_INTERVAL} &
 else
 	export HEROKU_STATSD_PORT=$(get-random-port)
-	statsdaemon -address ":${HEROKU_STATSD_PORT}" -export-url ${HEROKU_METRICS_URL} -instance ${DYNO} &
+	./bin/statsdaemon -address ":${HEROKU_STATSD_PORT}" -export-url ${HEROKU_METRICS_URL} -instance ${DYNO} &
 fi  
