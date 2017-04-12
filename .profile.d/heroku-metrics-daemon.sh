d66649ff496c3b83f82afa9baca8f178dabeba4c
Joe Kutner <jpkutner@gmail.com>
2017-04-12 14:03:18 -0500

    Skip profile.d in a one-off dyno

--- .profile.d/heroku-metrics-daemon.sh
+++ .profile.d/heroku-metrics-daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # don't do anything if we don't have a metrics url.
-if [[ -z "$HEROKU_METRICS_URL" ]]; then
+if [[ -z "$HEROKU_METRICS_URL" ]] || [[ "${DYNO}" = *run.* ]]; then
     return 0
 fi
 
