9c1659e964d7d8c4fb96efc57deb276166fafef5
BuildpackTester <jpkutner@gmail.com>
2017-12-04 16:55:03 -0500

    Remove support for JVM metrics agent, which has moved to the heroku/jvm buildpack

--- .profile.d/heroku-metrics-daemon.sh
+++ .profile.d/heroku-metrics-daemon.sh
@@ -5,51 +5,42 @@ if [[ -z "$HEROKU_METRICS_URL" ]] || [[ "${DYNO}" = run\.* ]]; then
     return 0
 fi
 
-# heroku-metrics-agent.jar is added in bin/compile
-if [[ -f ${HOME}/bin/heroku-metrics-agent.jar ]]; then
-    if [[ -f build.sbt ]] || # Scala
-       [[ -d target/resolution-cache ]]; then # Scala (sbt-heroku)
-        export JAVA_OPTS="-javaagent:${HOME}/bin/heroku-metrics-agent.jar ${JAVA_OPTS}"
-    else
-        export JAVA_TOOL_OPTIONS="-javaagent:${HOME}/bin/heroku-metrics-agent.jar ${JAVA_TOOL_OPTIONS}"
-    fi
+
+STARTTIME=$(date +%s)
+BUILD_DIR=/tmp
+
+DOWNLOAD_URL=$(curl --retry 3 -s https://agentmon-releases.s3.amazonaws.com/latest)
+if [ -z "${DOWNLOAD_URL}" ]; then
+    echo "!!!!! Failed to find latest agentmon. Please report this as a bug. Metrics collection will be disabled this run."
+    return 0
+fi
+
+BASENAME=$(basename "${DOWNLOAD_URL}")
+
+curl -L --retry 3 -s -o "${BUILD_DIR}/${BASENAME}" "${DOWNLOAD_URL}"
+
+# Ensure the bin folder exists, if not already.
+mkdir -p "${BUILD_DIR}/bin"
+
+# Extract agentmon release
+tar --warning=no-unknown-keyword -C "${BUILD_DIR}/bin" -zxf "${BUILD_DIR}/${BASENAME}"
+chmod +x "${BUILD_DIR}/bin/agentmon"
+
+ELAPSEDTIME=$(($(date +%s) - STARTTIME))
+echo "agentmon setup took ${ELAPSEDTIME} seconds"
+
+AGENTMON_FLAGS=("-statsd-addr=:${PORT}")
+
+if [[ "${AGENTMON_DEBUG}" = "true" ]]; then
+    AGENTMON_FLAGS+=("-debug")
+fi
+
+if [[ -x "${BUILD_DIR}/bin/agentmon" ]]; then
+    (while true; do
+        ${BUILD_DIR}/bin/agentmon "${AGENTMON_FLAGS[@]}" "${HEROKU_METRICS_URL}"
+        echo "agentmon completed with status=${?}. Restarting"
+        sleep 1
+    done) &
 else
-    STARTTIME=$(date +%s)
-    BUILD_DIR=/tmp
-
-    DOWNLOAD_URL=$(curl --retry 3 -s https://agentmon-releases.s3.amazonaws.com/latest)
-    if [ -z "${DOWNLOAD_URL}" ]; then
-        echo "!!!!! Failed to find latest agentmon. Please report this as a bug. Metrics collection will be disabled this run."
-        return 0
-    fi
-
-    BASENAME=$(basename "${DOWNLOAD_URL}")
-
-    curl -L --retry 3 -s -o "${BUILD_DIR}/${BASENAME}" "${DOWNLOAD_URL}"
-
-    # Ensure the bin folder exists, if not already.
-    mkdir -p "${BUILD_DIR}/bin"
-
-    # Extract agentmon release
-    tar --warning=no-unknown-keyword -C "${BUILD_DIR}/bin" -zxf "${BUILD_DIR}/${BASENAME}"
-    chmod +x "${BUILD_DIR}/bin/agentmon"
-
-    ELAPSEDTIME=$(($(date +%s) - STARTTIME))
-    echo "agentmon setup took ${ELAPSEDTIME} seconds"
-
-    AGENTMON_FLAGS=("-statsd-addr=:${PORT}")
-
-    if [[ "${AGENTMON_DEBUG}" = "true" ]]; then
-        AGENTMON_FLAGS+=("-debug")
-    fi
-
-    if [[ -x "${BUILD_DIR}/bin/agentmon" ]]; then
-        (while true; do
-            ${BUILD_DIR}/bin/agentmon "${AGENTMON_FLAGS[@]}" "${HEROKU_METRICS_URL}"
-            echo "agentmon completed with status=${?}. Restarting"
-            sleep 1
-        done) &
-    else
-        echo "No agentmon executable found. Not starting."
-    fi
+    echo "No agentmon executable found. Not starting."
 fi
